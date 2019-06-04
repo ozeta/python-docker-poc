439bfe58a3874d3d8aa0799795aef2bb8398550c
Marco Carrozzo <ozeta86@gmail.com>
2019-06-04 22:57:01 +0200

    take off!

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import logging
 import requests
 import sys, getopt
 
+# docker run --entrypoint python ./main.py ciaone --name pyworld pyworld:1.0
 def main(argv):
     response = requests.get("https://petstore.swagger.io/v2/store/inventory")
     print("status: {}".format(response.status_code))
